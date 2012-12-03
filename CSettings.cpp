da813f8061b5c24c4d67a15ee10419090bcd225d
Alexandros Nikolopoulos <alxnik@gmail.com>
2012-12-03 18:48:41 +0200

    Support for a settings file

---
+++ CSettings.cpp
@@ -0,0 +1,182 @@
+/*
+ * Copyright (c) 2012 Alexandros Nikolopoulos <alxnik@gmail.com>
+ *
+ * This program is free software; you can redistribute  it and/or modify it
+ * under  the terms of  the GNU General  Public License as published by the
+ * Free Software Foundation;  either version 2 of the  License, or (at your
+ * option) any later version.
+ */
+
+#include "CSettings.h"
+#include "CLog.h"
+
+CSettings::CSettings()
+{
+	m_SettingsFile = 0;
+	pthread_mutex_init(&FileMutex, NULL);
+}
+
+CSettings::~CSettings()
+{
+}
+
+int
+CSettings::LoadFile(string filename)
+{
+	m_FileName = filename;
+
+	if(!OpenFile())
+		return false;
+
+	XMLElement *Main = m_SettingsFile->FirstChildElement("fpd");
+	if(!Main)
+		return false;
+
+	XMLElement *Inputs = Main->FirstChildElement("inputs");
+	XMLElement *Outputs = Main->FirstChildElement("outputs");
+	if(!Inputs || !Outputs)
+		return false;
+
+	if(!ParseInputs(Inputs))
+		return false;
+	if(!ParseOutputs(Outputs))
+		return false;
+
+	return true;
+}
+
+int
+CSettings::ParseInputs(XMLElement *Inputs)
+{
+	// Parse Inputs
+	XMLElement *Input = Inputs->FirstChildElement("input");
+
+	int NumberOfInputs = 0;
+	while(Input)
+	{
+		InputContainer InputParse;
+		NumberOfInputs++;
+
+		if(!Input->Attribute("type") || !Input->Attribute("UUID"))
+			return false;
+
+		InputParse.uuid = Input->Attribute("UUID");
+		InputParse.type = Input->Attribute("type");
+
+		XMLElement *Interface = Input->FirstChildElement("iface");
+		if(!Interface || !Interface->Attribute("type"))
+			return false;
+
+		InputParse.iface = Interface->Attribute("type");
+
+		XMLNode *iSets = Interface->FirstChild();
+		while(iSets)
+		{
+			if(iSets->Value() || iSets->ToElement()->GetText())
+			{
+				InputParse.ifaceSettings[iSets->Value()] = iSets->ToElement()->GetText();
+				syslog(LOG_INFO, "parsed [%s]=[%s]", iSets->Value(), InputParse.ifaceSettings[iSets->Value()].c_str());
+			}
+
+			iSets = iSets->NextSibling();
+		}
+
+
+
+		XMLElement *Sensor = Input->FirstChildElement("sensors")->FirstChildElement("sensor");
+		int NumOfSensors = 0;
+		while(Sensor)
+		{
+			if(!Sensor->Attribute("address"))
+				return false;
+			NumOfSensors++;
+			InputParse.sensors.push_back(atol(Sensor->Attribute("address")));
+			Sensor = Sensor->NextSiblingElement("sensor");
+		}
+		InputsDB.push_back(InputParse);
+
+		syslog(LOG_INFO, "Parsed %s (%s - %s) with %d sensors\n", InputParse.uuid.c_str(), InputParse.type.c_str(),
+				InputParse.iface.c_str(), NumOfSensors);
+
+		Input = Input->NextSiblingElement("input");
+	}
+	syslog(LOG_INFO, "Parsed %d inputs\n", NumberOfInputs);
+
+	return true;
+}
+
+int
+CSettings::ParseOutputs(XMLElement *Outputs)
+{
+	// Parse Outputs
+	XMLElement *Output = Outputs->FirstChildElement("output");
+
+	int NumberOfOutputs = 0;
+	while(Output)
+	{
+		OutputContainer OutputParse;
+		NumberOfOutputs++;
+
+		if(!Output->Attribute("name"))
+		{
+			syslog(LOG_ERR, "Output entry has no name\n");
+			return false;
+		}
+
+		// Check if there is a duplicate name
+		OutputParse.name = Output->Attribute("name");
+		for(list<OutputContainer>::iterator i=OutputsDB.begin(); i != OutputsDB.end(); ++i)
+		{
+			if(OutputParse.name.compare((*i).name) == 0)
+			{
+				syslog(LOG_ERR, "Duplicate output name\n");
+				return false;
+			}
+		}
+
+
+		OutputParse.type = Output->Attribute("type");
+
+		XMLNode *OutSettings = Output->FirstChild();
+
+		while(OutSettings)
+		{
+			if(OutSettings->Value() || OutSettings->ToElement()->GetText())
+			{
+				OutputParse.settings[OutSettings->Value()] = OutSettings->ToElement()->GetText();
+				syslog(LOG_INFO, "parsed [%s]=[%s]", OutSettings->Value(), OutputParse.settings[OutSettings->Value()].c_str());
+			}
+
+			OutSettings = OutSettings->NextSibling();
+		}
+
+		OutputsDB.push_back(OutputParse);
+
+		Output = Output->NextSiblingElement("output");
+	}
+
+
+	syslog(LOG_INFO, "Parsed %d outputs\n", NumberOfOutputs);
+
+	return true;
+}
+
+int
+CSettings::OpenFile(void)
+{
+	if(m_SettingsFile)
+		return true;
+
+	m_SettingsFile = new XMLDocument();
+
+	if(m_SettingsFile->LoadFile(m_FileName.c_str()) != XML_NO_ERROR)
+	{
+		syslog(LOG_ERR, "Error Opening %s: %d\n", m_FileName.c_str(), m_SettingsFile->ErrorID());
+
+		delete m_SettingsFile;
+		m_SettingsFile = 0;
+		return false;
+	}
+
+	return true;
+}
---
+++ CSettings.h
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2012 Alexandros Nikolopoulos <alxnik@gmail.com>
+ *
+ * This program is free software; you can redistribute  it and/or modify it
+ * under  the terms of  the GNU General  Public License as published by the
+ * Free Software Foundation;  either version 2 of the  License, or (at your
+ * option) any later version.
+ */
+
+#ifndef CSETTINGS_H_
+#define CSETTINGS_H_
+
+
+#include <list>
+#include <map>
+#include <string>
+#include <sstream>
+using namespace std;
+
+
+#include "tinyxml/tinyxml2.h"
+using namespace tinyxml2;
+
+#include "fpd.h"
+
+typedef struct
+{
+	string		uuid;
+	string		type;
+	string		iface;
+
+	map<string, string>		ifaceSettings;
+	list<int> 				sensors;
+} InputContainer;
+
+typedef struct
+{
+	string 	name;
+	string	type;
+	map<string,string>	settings;
+} OutputContainer;
+
+
+class CSettings {
+public:
+	CSettings();
+	virtual ~CSettings();
+
+	int LoadFile(string filename);
+	list<InputContainer> 	InputsDB;
+	list<OutputContainer> 	OutputsDB;
+
+private:
+	string m_FileName;
+	XMLDocument *m_SettingsFile;
+	pthread_mutex_t FileMutex;
+
+
+	int OpenFile(void);
+
+	int ParseInputs(XMLElement *Inputs);
+	int ParseOutputs(XMLElement *Outputs);
+
+};
+
+#endif /* CSETTINGS_H_ */
